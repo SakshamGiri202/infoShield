d15d1b14a444ddd67db164363c6bbf4604487d1c
Ashu-Harshitha <dharshitha2712@gmail.com>
2025-09-07 20:41:35 +0530

    frontend started

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+// frontend/src/Home.js
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>This is your new React component.</p>
+    </div>
+  );
+}
+
+export default Home;
