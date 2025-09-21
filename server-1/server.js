fd834784d63f39727df8a139a722cbd13c75f592
Ashu-Harshitha <dharshitha2712@gmail.com>
2025-09-21 22:02:33 +0530

    integrating

--- server-1/server.js
+++ server-1/server.js
@@ -14,4 +14,5 @@ app.get("/", (req, res) => res.send("Hello Backend!"));
 app.use('/api/users', userRoutes);
 app.use('/api/analysis', analysisRoutes);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
